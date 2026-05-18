aa068c45d00108afb0ddbef780f9308f6146c7fa
Nintendiuk <oleksii.rachynskyi@gmail.com>
2026-05-18 20:18:09 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -7,6 +7,12 @@ from .views import (
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
+    CarCreateView,
+    ManufacturerCreateView,
+    CarUpdateView,
+    ManufacturerUpdateView,
+    CarDeleteView,
+    ManufacturerDeleteView,
 )
 
 urlpatterns = [
@@ -16,12 +22,29 @@ urlpatterns = [
         ManufacturerListView.as_view(),
         name="manufacturer-list",
     ),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-    ),
+    path("cars/", CarListView.as_view(),
+         name="car-list"),
+    path("cars/<int:pk>/", CarDetailView.as_view(),
+         name="car-detail"),
+    path("drivers/", DriverListView.as_view(),
+         name="driver-list"),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(),
+         name="driver-detail"),
+    path("car/create", CarCreateView.as_view(),
+         name="car-create"),
+    path("Manufacturer/create",
+         ManufacturerCreateView.as_view(),
+         name="manufacturer-create"),
+    path("car-list/<int:pk>/update", CarUpdateView.as_view(),
+         name="car-update"),
+    path("Manufacturer-list/<int:pk>/update",
+         ManufacturerUpdateView.as_view(),
+         name="manufacturer-update"),
+    path("car-list/<int:pk>/delete", CarDeleteView.as_view(),
+         name="car-delete"),
+    path("Manufacturer-list/<int:pk>/delete",
+         ManufacturerDeleteView.as_view(),
+         name="manufacturer-delete")
 ]
 
 app_name = "taxi"
